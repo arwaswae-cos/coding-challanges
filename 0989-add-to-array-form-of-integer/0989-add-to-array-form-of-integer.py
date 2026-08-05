0cf3c293db0cfdc7cdf9748717ab3e44554c36bb
Arwa <tinwalaarwa8@gmail.com>
2026-08-06 00:17:39 +0530

    Time: 23 ms (32.85%), Space: 19.6 MB (75.62%) - LeetHub

--- 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.py
+++ 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.py
@@ -1,8 +1,16 @@
-import sys
-sys.set_int_max_str_digits(10000)
 class Solution:
     def addToArrayForm(self, num: List[int], k: int) -> List[int]:
-        n, length = 0, len(num)
-        for i in range(length):
-            n = n*10 + num[i]
-        return list(map(int, str(n+k)))
+        carry, n = 0, len(num)
+        for i in range(1,n+1):
+            p = num[-i]+k%10+carry 
+            num[-i] = p if p<10 else p%10
+            carry = p//10
+            k = k//10 
+        if k!=0:
+            lst = list(map(int, str(k+carry)))
+            num = lst + num
+            carry = 0
+        if carry != 0:
+            num = [carry] +num
+    
+        return num
