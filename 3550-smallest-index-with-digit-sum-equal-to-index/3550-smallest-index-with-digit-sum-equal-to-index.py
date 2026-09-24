528ac7612960111cae4ad853900cb115f57807a7
Arwa <tinwalaarwa8@gmail.com>
2026-09-24 22:09:10 +0530

    Time: 7 ms (16.46%), Space: 19.1 MB (93.24%) - LeetHub

--- 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.py
+++ 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.py
@@ -1,9 +1,7 @@
 class Solution:
-    def findSum(self, num):
-        return sum(list(map(int,str(num))))
     def smallestIndex(self, nums: List[int]) -> int:
         for i in range(len(nums)):
-            if self.findSum(nums[i]) == i:
+            if sum(list(map(int,str(nums[i])))) == i:
                 return i
         return -1
         
